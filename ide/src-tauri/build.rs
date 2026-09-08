b643e980e1e9dd8c2884eb8f875582788b294521
kira568 <kira568@users.noreply.github.com>
2026-09-08 06:18:10 +0300

    Fix Linux CI: use CARGO_MANIFEST_DIR for resource paths + pre-copy binaries in workflow

--- ide/src-tauri/build.rs
+++ ide/src-tauri/build.rs
@@ -5,18 +5,28 @@ fn main() {
     tauri_build::build();
 
     let ext = if cfg!(target_os = "windows") { ".exe" } else { "" };
-    let res_dir = PathBuf::from("resources");
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    let res_dir = manifest_dir.join("resources");
     let _ = fs::create_dir_all(&res_dir);
 
-    let rakc_src = PathBuf::from(format!("../../target/release/rakc{}", ext));
-    let rakpkg_src = PathBuf::from(format!("../../target/release/rakpkg{}", ext));
+    let rakc_src = manifest_dir.join(format!("../../target/release/rakc{}", ext));
+    let rakpkg_src = manifest_dir.join(format!("../../target/release/rakpkg{}", ext));
 
     if rakc_src.exists() {
-        let _ = fs::copy(&rakc_src, res_dir.join("rakc"));
-        println!("cargo:rerun-if-changed={}", rakc_src.display());
+        match fs::copy(&rakc_src, res_dir.join("rakc")) {
+            Ok(_) => println!("cargo:warning=Copied rakc to resources/"),
+            Err(e) => println!("cargo:warning=Failed to copy rakc: {}", e),
+        }
+    } else {
+        println!("cargo:warning=rakc binary not found at {}", rakc_src.display());
     }
+
     if rakpkg_src.exists() {
-        let _ = fs::copy(&rakpkg_src, res_dir.join("rakpkg"));
-        println!("cargo:rerun-if-changed={}", rakpkg_src.display());
+        match fs::copy(&rakpkg_src, res_dir.join("rakpkg")) {
+            Ok(_) => println!("cargo:warning=Copied rakpkg to resources/"),
+            Err(e) => println!("cargo:warning=Failed to copy rakpkg: {}", e),
+        }
+    } else {
+        println!("cargo:warning=rakpkg binary not found at {}", rakpkg_src.display());
     }
 }
